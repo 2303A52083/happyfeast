77b5d53671291d8ed68301888cc359092c5a986b
Humera Nuzhat <2303a52083@sru.edu.in>
2026-04-07 12:36:12 +0530

    Update Header.jsx

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className={style.header}>
       <div className={style.headerContent}>
-        <h2>Order your faourite food here</h2>
+        <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes and variety of cuisines.</p>
         <a href="#ExploreMenu"><button>View Menu</button></a>
       </div>
